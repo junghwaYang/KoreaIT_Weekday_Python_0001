43ad5fd2cb2ea5ef6c2107f9ed37318cc174cd7f
tare <siltarre@gmail.com>
2025-07-03 19:06:07 +0900

    학습

--- Programming/Python/Example/Practice/Practice_06/P01Practice_06.py
+++ Programming/Python/Example/Practice/Practice_06/P01Practice_06.py
@@ -42,4 +42,25 @@ Ex)
 
 # Practice 6
 def start(args):
-	pass
+	nNumLines = int(input("정수를 입력하세요:"))
+	print()
+	
+	nWidth_Max = (nNumLines * 2) - 1
+	
+	for i in range(0, nNumLines):  # i의 범위
+		nCenter = nWidth_Max // 2
+		
+		for j in range(0, nWidth_Max):  # j의 범위
+			bIsStar = j >= nCenter >= j
+			print("{0}".format("*" if bIsStar else " "), end = "")
+		print()
+	
+	print()
+	
+	for i in range(0, nNumLines)[::-1]:  # i의 범위 [::-1] 은 slice라고 한다
+		nCenter = nWidth_Max // 2
+		
+		for j in range(0, nWidth_Max):  # j의 범위
+			bIsStar = j >= nCenter >= j
+			print("{0}".format("*" if bIsStar else " "), end = "")
+		print()
